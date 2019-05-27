281b08e7bae8665a2eaec2ce4bb8de134c29b8ea
unknown <hanohj@gmail.com>
2019-05-27 15:06:54 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const btn = document.createElement('button');
+btn.textContent = 'Press Me';
+document.body.appendChild(btn);
+btn.addEventListener('click' , function(){
+    fetchData('https://swapi.co/api/planets');
+});
+const output = document.createElement('div');
+document.body.appendChild(output);
+
+
+function fetchData(url) {
+    fetch(url).then(function(rep){
+        // console.log(rep)
+        return rep.json();
+    }).then(function(data){
+        output.textContent = `${data.count} results found`;
+        if (data.next) {
+            const btn2 = document.createElement('button');
+            btn2.textContent = 'next';
+            output.appendChild(btn2);
+            btn2.addEventListener('click' , function(){
+                fetchData(data.next);
+            });
+        }
+        console.log(data);
+    })
+}
+
