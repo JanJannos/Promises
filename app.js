08bef51af20d2b8f9f2a3b2575935ee816890895
unknown <hanohj@gmail.com>
2019-05-28 11:41:33 +0300

    New Commit

--- app.js
+++ app.js
@@ -2,20 +2,35 @@
 /* Hanoh Jan - 2019     */
 
 
+// Now adding a catch block to the Promise
+
+//const btn = document.createElement('button');
+//btn.textContent = 'Press Me';
+//document.body.appendChild(btn);
+//btn.addEventListener('click' , function(){
+//    fetchAll('https://swapi.co/api/planets' , []).then(function(planets) {
+//       outputPlanets(planets);
+//    });
+//});
+
 const btn = document.createElement('button');
 btn.textContent = 'Press Me';
 document.body.appendChild(btn);
 btn.addEventListener('click' , function(){
-    // fetchData('https://swapi.co/api/planets');
     fetchAll('https://swapi.co/api/planets' , []).then(function(planets) {
-       // console.log(planets);
-        outputPlanets(planets);
+            outputPlanets(planets);
+        }).catch(function(error){       // added a catch
+        console.log(error);
     });
 });
 
 
+
+
 function fetchAll(url , planets){
      return new Promise(function(resolve, reject) {
+          // If you want to catach , uncomment the following line
+          // throw 'UH HO !!!';
           return fetch(url).then(function(rep){
              return rep.json(); 
           }).then(function(data) {
@@ -37,7 +52,9 @@ function fetchAll(url , planets){
            }
        
              resolve(planets);
-          });
+          }).catch(function(error){              
+              console.log(error);
+          });  // end catch , then , promise
       });
 }
 
@@ -45,17 +62,26 @@ const output = document.createElement('div');
 document.body.appendChild(output);
 
 function outputPlanets(data) {
+    // we will use InnerHTML instead
+    // output.textContent = `${data.length} results found`;
+    output.innerHTML = `<h1>${data.length} results found</h1>`;
     data.forEach(function(item) {
         console.log(item);
         const div = document.createElement('div');
         div.textContent = item.name;
-        const ul = document.createElement('ul');
-        for(let x = 0;x < item.films.length; x++){
-            let li = document.createElement('li');
-            li.textContent = item.films[x];
-            ul.appendChild(li);
+        let span = document.createElement('span');
+        span.textContent = ` ${item.films.length} films found`;
+        div.appendChild(span);        
+        if (item.films.length > 0) {
+            const ul = document.createElement('ul');
+            for(let x = 0;x < item.films.length; x++){
+                let li = document.createElement('li');
+                li.textContent = item.films[x];
+                ul.appendChild(li);
+            }
+            div.appendChild(ul);
         }
-        div.appendChild(ul);
+
         output.appendChild(div); 
     });
     console.log(data);
@@ -68,7 +94,8 @@ function fetchData(url) {
         // console.log(rep)
         return rep.json();
     }).then(function(data){
-        output.textContent = `${data.count} results found`;
+        // we lost the next line of the results , so now we move it up to "outputPlanets"
+        // output.textContent = `${data.count} results found`;
         if (data.next) {
             const btn2 = document.createElement('button');
             btn2.textContent = 'next';
